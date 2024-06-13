494e4da886414c43c1e4bc3315028aa059990304
saraseyfii <seyfisara68@gmail.com>
2024-06-13 12:42:50 +0330

    Add files via upload

---
+++ Aval.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+{
+	int n;
+	cin >> n;
+	int mod N = 0;
+	for (int i = 2; i < (a / 2); i++) {
+		if (n%i == 0) {
+			mod N++;
+		}
+	}
+	if (modN == 0) {
+		cout << "primary";
+	}
+	return 0;
+}
